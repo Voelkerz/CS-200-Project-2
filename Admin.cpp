f81ab7b4eb2e2b9840fccaa94b73db4a72e8c9bb
UppNXt <37936253+UppNXt@users.noreply.github.com>
2019-04-04 11:29:50 -0400

    Add files via upload

---
+++ Admin.cpp
@@ -0,0 +1,8 @@
+//Admin.cpp
+#include"Admin.h"
+using namespace std;
+
+void Admin::setID(string i)
+{
+	id = id + "A";
+};
---
+++ Banker.cpp
@@ -0,0 +1,8 @@
+//Banker.cpp
+using namespace std;
+#include"Banker.h"
+
+void Banker::setID(string i)
+{
+	id = i + "B";
+};
