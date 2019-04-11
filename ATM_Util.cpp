204154c14dc47897c0f8cba00bd09449979c51b5
seblankenship <33261082+seblankenship@users.noreply.github.com>
2019-04-10 22:05:35 -0400

    Add files via upload

--- ATM_Util.cpp
+++ ATM_Util.cpp
@@ -1,2 +1,61 @@
-// ATM_Util.cpp
+//ATM_Util.cpp
 #include "ATM_Util.h"
+
+ATM_Util::ATM_Util()
+{
+	
+}
+
+bool ATM_Util::util_main()
+{
+	
+}
+
+int ATM_Util::menu()
+{
+	int option;
+	
+	system("CLS");
+	
+	cout <<"\t\tATM Utility Menu"<<endl
+		 <<"\t\t=========================="
+		 <<"\t\t1. Make a transfer"
+		 <<"\t\t2. Print account balance"
+		 <<"\t\t3. Print account info"
+		 <<"\t\t4. Print customer transaction history"
+		 <<"\t\t5. Logout"
+		 <<"\tChoose an option"<<endl;
+	cin >>option;
+		
+	return option;
+}
+
+int ATM_Util::transfer()
+{
+	
+}
+
+int ATM_Util::printBalance()
+{
+	
+}
+
+int ATM_Util::printAccountInfo()
+{
+	
+}
+
+string ATM_Util::customerTransHistory()
+{
+	
+}
+
+string ATM_Util::changePassword()
+{
+	
+}
+
+void ATM_Util::foo()
+{
+	
+}
--- ATM_Util.h
+++ ATM_Util.h
@@ -1,15 +1,26 @@
-// ATM_Util.h
-// This will be the controlling system for all customer user commands
-#include <iostream>
-using namespace std;
+//ATM_Util.h
+//This will be the controlling system for all cystomer user commands
+#include<iostream>
 #include "Customer.h"
+using namespace std;
 
 #ifndef ATM_UTIL_H
 #define ATM_UTIL_H
-class ATM_Util {
+
+class ATM_Util
+{
 	public:
+		ATM_Util();
 		static bool util_main();
 		static int menu();
-		virtual void foo()=0;
+		int transfer();
+		int printBalance();
+		int printAccountInfo();
+		string customerTransHistory();
+		string changePassword();
+		virtual void foo() = 0;
+	private:
+		
 };
+
 #endif
