3e64721b2fcdf2a99758fe32f3dc2f2c736954cc
UppNXt <37936253+UppNXt@users.noreply.github.com>
2019-04-12 22:43:21 -0400

    Update Banker_Util.h

--- Banker_Util.h
+++ Banker_Util.h
@@ -3,13 +3,18 @@
 #include <iostream>
 using namespace std;
 #include "Banker.h"
+#include "Login.h"
+#include "Admin_Util.h"
+#include "Transactions.h"
 
 #ifndef BANKER_UTIL_H
 #define BANKER_UTIL_H
 class Banker_Util {
 	public:
-		static bool util_main();
-		static int menu();
+		Banker_Util();
+		static bool util_main(int, User* [], int, int&, Employee* [], int, int&, Admin*, int, int&, Banker*, int, int&, Customer*, int, int&);
+		static void menu(int, User*, int, int, Customer*, int);
 		virtual void foo()=0;
+		void changePassword();
 };
 #endif
